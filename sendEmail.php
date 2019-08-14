b2708d3f98202ce3626044346d84b405504af1a7
Marlon <m.c4n1as@gmail.com>
2019-08-14 10:19:02 -0700

    created sendEmail.html and sendEmail.php
    Created a web form to create an email that will be sent to the list of customers that are saved in the elvis store database email_list table. The sendEmail.php is the script that will automate the sending of the email to all of the customers.

---
+++ sendEmail.php
@@ -0,0 +1,35 @@
+<html>
+    <title>Elvis Email PHP script</title>
+    <body>
+        <?php
+            $from = 'finding_marlon@yahoo.com';
+            $subject = $_POST['subject'];
+            $text = $_POST['elvisEmail'];
+
+            $dbc = mysqli_connect('localhost','root','','elvis_store')
+            or die('Error connecting to MySQL server');
+
+            $query = "SELECT * FROM email_list";
+            $result = mysqli_query($dbc, $query)
+            or die('Error querying database.');
+
+            while($row = mysqli_fetch_array($result)){
+                $first_name = $row['first_name'];
+                $last_name = $row['last_name'];
+
+                $msg = "Dear $first_name $last_name,\n $text";
+
+                $to = $row['email'];
+
+                mail($to, $subject, $msg, 'From: '. $from);
+
+                echo 'Email sent to: '. $to.'<br>'; 
+
+            }
+            
+            mysqli_close($dbc);
+            //echo $row['first_name']. ' '. $row['last_name']. ' '. $row['email']. '<br>';
+            echo '<a href = "sendEmail.html">Go back</a>'
+        ?>
+    </body>
+</html>
