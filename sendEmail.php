7b21f853cebb1c473f186f5523c5b5c52e93c6ee
Marlon <m.c4n1as@gmail.com>
2019-08-18 04:25:10 -0700

    Adding form validation to sendEmail.php
    Created if statements to check if subject fields and text field of the email have content. This prevents sending blank emails.

--- sendEmail.php
+++ sendEmail.php
@@ -5,25 +5,29 @@
             $from = 'elmer@makemeelvis.com';
             $subject = $_POST['subject'];
             $text = $_POST['elvisEmail'];
+            if(!empty($subject)){
+                if(!empty($text)){
+                        $dbc = mysqli_connect('localhost','root','','elvis_store')
+                        or die('Error connecting to MySQL server');
 
-            $dbc = mysqli_connect('localhost','root','','elvis_store')
-            or die('Error connecting to MySQL server');
+                        $query = "SELECT * FROM email_list";
+                        $result = mysqli_query($dbc, $query)
+                        or die('Error querying database.');
 
-            $query = "SELECT * FROM email_list";
-            $result = mysqli_query($dbc, $query)
-            or die('Error querying database.');
+                        while($row = mysqli_fetch_array($result)){
+                            $first_name = $row['first_name'];
+                            $last_name = $row['last_name'];
 
-            while($row = mysqli_fetch_array($result)){
-                $first_name = $row['first_name'];
-                $last_name = $row['last_name'];
+                            $msg = "Dear $first_name $last_name,\n $text";
 
-                $msg = "Dear $first_name $last_name,\n $text";
+                            $to = $row['email'];
 
-                $to = $row['email'];
+                            mail($to, $subject, $msg, 'From: '. $from);
 
-                mail($to, $subject, $msg, 'From: '. $from);
-
-                echo 'Email sent to: '. $to.'<br>'; 
+                            echo 'Email sent to: '. $to.'<br>';
+                        }
+                }
+             
 
             }
             
