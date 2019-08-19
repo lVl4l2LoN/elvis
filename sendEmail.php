367dae65c81eb3864e150c453e90969a99675966
Marlon <m.c4n1as@gmail.com>
2019-08-19 04:04:30 -0700

    Updated sendEmail.php. Added isset test and sticky fields
    added if statement with isset clause to check if the submit value had been set to distinguish the event between displaying the form the first time or submitting an email with blank fields. Added echo statements to the value attribute of the  subject field and to the text area to make the form sticky.

--- sendEmail.php
+++ sendEmail.php
@@ -3,50 +3,55 @@
     <body>
         <h1>Make Me Elvis</h1>
         <?php
-            $from = 'elmer@makemeelvis.com';
-            $subject = $_POST['subject'];
-            $text = $_POST['elvisEmail'];
-            $output_form = false;
-            if(empty($subject)&& empty($text)){
-                echo'The subject and body are both empty.';
-                $output_form = true;
-            }
-            if(!(empty($subject))&& empty($text)){
-                echo'The body is empty.';
-                $output_form = true;
-            }
-            if(empty($subject)&& !(empty($text))){
-                echo' The subject is empty.';
-                $output_form = true;
-            }
-            if(!(empty($subject))&&!(empty($text))){
-                
-                $dbc = mysqli_connect('localhost','root','','elvis_store')
-                or die('Error connecting to MySQL server');
-
-                $query = "SELECT * FROM email_list";
-                $result = mysqli_query($dbc, $query)
-                or die('Error querying database.');
-
-                while($row = mysqli_fetch_array($result)){
-                    $first_name = $row['first_name'];
-                    $last_name = $row['last_name'];
-
-                    $msg = "Dear $first_name $last_name,\n $text";
-
-                    $to = $row['email'];
-
-                    mail($to, $subject, $msg, 'From: '. $from);
-
-                    echo 'Email sent to: '. $to.'<br>';
+            if(isset($_POST['submit'])){
+                $from = 'elmer@makemeelvis.com';
+                $subject = $_POST['subject'];
+                $text = $_POST['elvisEmail'];
+                $output_form = false;
+                if(empty($subject)&& empty($text)){
+                    echo'The subject and body are both empty.';
+                    $output_form = true;
+                }
+                if(!(empty($subject))&& empty($text)){
+                    echo'The body is empty.';
+                    $output_form = true;
+                }
+                if(empty($subject)&& !(empty($text))){
+                    echo' The subject is empty.';
+                    $output_form = true;
+                }
+                if(!(empty($subject))&&!(empty($text))){
                     
+                    $dbc = mysqli_connect('localhost','root','','elvis_store')
+                    or die('Error connecting to MySQL server');
+
+                    $query = "SELECT * FROM email_list";
+                    $result = mysqli_query($dbc, $query)
+                    or die('Error querying database.');
+
+                    while($row = mysqli_fetch_array($result)){
+                        $first_name = $row['first_name'];
+                        $last_name = $row['last_name'];
+
+                        $msg = "Dear $first_name $last_name,\n $text";
+
+                        $to = $row['email'];
+
+                        mail($to, $subject, $msg, 'From: '. $from);
+
+                        echo 'Email sent to: '. $to.'<br>';
+                        
+                    
+                    
+                    }   
                 
-                
-                }   
-             
-                mysqli_close($dbc);
-    //echo $row['first_name']. ' '. $row['last_name']. ' '. $row['email']. '<br>';
-                //echo '<a href = "sendEmail.html">Go back</a>';
+                    mysqli_close($dbc);
+        //echo $row['first_name']. ' '. $row['last_name']. ' '. $row['email']. '<br>';
+                    //echo '<a href = "sendEmail.html">Go back</a>';
+                }
+            }
+            else{
+                $output_form = true;
             }
 
           if($output_form){         
@@ -54,9 +59,10 @@
 
           <form method ="POST" action = "<?php echo $_SERVER['PHP_SELF']; ?>">
             <label for="subject">Subject of email:</label><br>
-            <input type="text" id="subject" name="subject" size="60"><br>
+            <input type="text" id="subject" name="subject" size="60"value="<?php echo $subject; ?>"><br>
             <label for="elvisMail">Body of Email: </label><br>
-            <textarea id="elvisEmail" name="elvisEmail" rows="8" cols="60"></textarea><br> 
+            <textarea id="elvisEmail" name="elvisEmail" rows="8" cols="60"> <?php echo $text; ?>
+            </textarea><br> 
             <input type="submit" name="submit" value="Submit">
           </form>
         <?php
