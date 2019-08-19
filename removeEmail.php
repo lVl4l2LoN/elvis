4f63308ac4b7f0ad15487070ff63d3497def7805
Marlon <m.c4n1as@gmail.com>
2019-08-19 07:19:01 -0700

    Updated removeEmail.php
    Redesigned removeEmail.php to remove emails by primary-key id instead of by email. Also redesigned the form into checkbox for removing emails instead of typing the email through the text field.

--- removeEmail.php
+++ removeEmail.php
@@ -1,22 +1,50 @@
 <html>
     <title>Make Me Elvis - Remove Email Page</title>
     <body>
+        <p>Please  select the email addresses to delete from the email list and click Remove</p>
+        <form method = "POST" action = "<?php echo $_SERVER['PHP_SELF']; ?>">
+            <?php
+                $dbc = mysqli_connect('localhost','root','','elvis_store')
+                or die('Error connecting to MySQL server.');
+                
+                if(isset($_POST['submit'])&& !empty($_POST['todelete'])){
 
-        <?php
-            $dbc = mysqli_connect('localhost','root','','elvis_store')
-            or die('Error connecting to MySQL server.');
+                    foreach($_POST['todelete'] as $delete_id){
+                        $query="DELETE FROM email_list WHERE id=$delete_id";
+                        mysqli_query($dbc,$query)
+                        or die('Error querying database');
+                        $delete_flag = true;
+                    }
+                         echo'Customer(s) removed: '.'<br>';
+                         
+                   
+                }
+                
 
-            $email = $_POST['email'];
+                //Display customer rows with checkboxes for deleting
 
-            $query = "DELETE FROM email_list WHERE email = '$email'";
+                $query = "SELECT * FROM email_list";
+                $result = mysqli_query($dbc, $query);
 
-            mysqli_query($dbc, $query)
-            or die('Error querying database.');
+                while($row=mysqli_fetch_array($result)){
+                    echo'<input type ="checkbox" value="' .$row['id'].'" name="todelete[]">';
+                    echo $row['first_name'];
+                    echo ' '.$row['last_name'];
+                    echo ' '.$row['email'];
+                    echo '<br>';
 
-            echo'Customer removed: '. $email.'<br>';
-            mysqli_close($dbc);
-            echo'<a href="removeEmail.html">Go Back</a>'
-        ?>
+                }
+                mysqli_close($dbc);
+                
+                
+
+                
+                
+                //echo'<a href="removeEmail.html">Go Back</a>'
+            ?>
+            <input type="submit" name="submit" value = "Remove">
+        </form>
+        
 
     </body>
 </html>
